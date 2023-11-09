4f0c452e948777a00f20eef7a67571c9afc1ad48
KO-d14 <ainajk@hawaii.edu>
2023-11-09 13:12:24 -1000

    Completed parts 1-5
    TODO:
    - Just clean up on the comments

---
+++ HackwichTen/Restaurant.swift
@@ -0,0 +1,46 @@
+//
+//  Restaurant.swift
+//  HackwichTen --- INTRO TO MAPKIT (IN CLASS ASSIGNMENT)
+//
+//  Created by Aina Kodaira on 11/9/23.
+//
+
+/*
+//Part 4: Create a Restaurant Object to hold data & display in annotation (7 points)
+1. Go to File-New-File
+2. Select, Cocoa Touch Class
+3. Name the new class, “Restaurant” and be sure it's a subclass of NSObject.
+*/
+import UIKit
+/*PART4-#4. Because we’re creating a map annotation, we need to:
+B. Adopt the MKAnnotation protocol
+*/
+import MapKit
+
+// PART4-#4.A)Import MapKit Framework
+class Restaurant: NSObject, MKAnnotation {
+  /*
+    5. Let’s set up the Restaurant class.
+    A.  Declare three variables that will hold data that the annotation will access (see lines 13, 14 & 15)
+    B.  Initialize the MKAnnotation object
+   */
+    let restaurantTitle: String?
+    let restaurantType: String
+    var coordinate: CLLocationCoordinate2D
+    
+    init(title: String, type: String, coordinate: CLLocationCoordinate2D)
+             {
+                 self.restaurantTitle = title
+                 self.restaurantType = type
+                 self.coordinate = coordinate
+                 
+                 super.init()
+                 
+             } // init closing bracket 
+    
+    var subtitle: String?
+        {
+            //Problem Set #3 Code Switch
+            return restaurantType
+        } // var subtitle closing bracket
+}
--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -1,19 +1,65 @@
 //
 //  ViewController.swift
-//  HackwichTen
+//  HackwichTen ---- INTRO TO MAPKIT (IN CLASS ASSIGNMENT)
 //
 //  Created by Aina Kodaira on 11/9/23.
 //
 
+
 import UIKit
+/*
+PART 1 - (setup)
+ Part 2: Set up Map Kit View Object (4 points)
+ #1. In the object library, drag and drop a “Map Kit View” on to your VC and stretch the view across your VC:
+#2. More set up for Map Kit View & import frameworks:
+A. >> code below
+B. import MapKit framework
+C. import CoreLocation framework (see line 10 & 11 below)
+*/
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+//>>>PART2-#2.A) Create an outlet for your Map Kit View and name it “mapView”
+    @IBOutlet weak var mapView: MKMapView!
+    
+//PART3-#1.A)Declare a constant “initialLocation” which is a CLLocation (or location object) and assign it to the latitude and longitude coordinates for the UHWO campus.
+    let initialLocation = CLLocation(latitude: 21.361888, longitude:
+         -158.056022)
+//PART3-#1.B) Declare a constant “regionRadius” which represents the radius in meters that the map will display from “initialLocation” which is the location of UHWO in this example.
+    let regionRadius = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//PART3-#3 displays the map when it first opens
+        centerMapOnLocation(location: initialLocation)
+        
+/*
+Part 5: Creating an Annotation to Display a Restaurant location on Map (3 points)
+        Keep in mind that in order to display an annotation, we need to set the annotation to display in viewDidLoad. This means that the annotation will show when the app initially loads.
+
+1. Go to your ViewController.swift file, and in viewDidLoad enter the following lines of code:
+*/
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American",
+                       coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude:
+                       -158.080098))
+                
+                mapView.addAnnotation(restaurantOne)
     }
+    
+//PART3-#2) Add the function,  centerMapOnLocation, underneath the viewDidLoad.
+//This function, centerMapOnLocation centers the map around the CLLocation by setting the coordinate region and map region.
+    func centerMapOnLocation(location:CLLocation)
+           {
+               let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
 
+               latitudinalMeters: CLLocationDistance(regionRadius),
+               longitudinalMeters: CLLocationDistance(regionRadius))
+               mapView.setRegion(coordinateRegion, animated: true)
 
+           }
 }
 
